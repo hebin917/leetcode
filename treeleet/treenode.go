dba524f36f3e03dbabc715a00f3045a4fb880da3
hebin <hebin@hebindeMacBook-Pro.local>
2019-04-23 22:51:18 +0800

    upgrade

--- treeleet/treenode.go
+++ treeleet/treenode.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"fmt"
+	"github.com/golang-collections/collections/queue"
 	"github.com/golang-collections/collections/stack"
-	)
+)
 
 /*
 二叉树特性：
@@ -55,55 +56,18 @@ func InitTreeNode() (TreeNode) {
 	root.right.right = NewNode(7)
 
 	root.left.left.left = NewNode(8)
+	root.left.left.right = NewNode(9)
+
+	root.left.right.left= NewNode(10)
+	root.left.right.right= NewNode(11)
+
+	root.right.left.left = NewNode(12)
+	root.right.left.right = NewNode(13)
+
+	root.right.right.left= NewNode(14)
+	root.right.right.right= NewNode(15)
+
 	return root
-}
-
-//----------------------------------------------------------------------------------------------
-
-/**
-计算tree的最大height：沿着根节点遍历到最长的叶子节点的长度
- */
-func height(root *TreeNode) int {
-	if root == nil {
-		return 0;
-	} else {
-		llen := height(root.left)
-		rlen := height(root.right)
-		if llen < rlen {
-			return rlen + 1
-		} else {
-			return llen + 1
-		}
-	}
-}
-
-/*
-按照层级打印： breadth first traversal： 即广度优先算法
-1. 先找出tree的深度
-2. 然后根据层级来进行打印
-3. 层级打印： printGivenLevel ： 递归基：level=1,然后打印root.data
- */
-func printLevel(root *TreeNode) {
-	if root == nil {
-		return
-	}
-	len := height(root)
-
-	for i := 1; i <= len; i++ {
-		printGivenLevel(root, i)
-	}
-}
-
-func printGivenLevel(root *TreeNode, level int) {
-	if root == nil {
-		return
-	}
-	if level == 1 {
-		print("%d ", root.data)
-	} else {
-		printGivenLevel(root.left, level-1)
-		printGivenLevel(root.right, level-1)
-	}
 }
 
 //----------------------------------------------------------------------------------------------
@@ -453,20 +417,20 @@ func printlevel(root *TreeNode, level int) {
 
 }
 
-func printGivenLev(root *TreeNode, level int,ftr bool) {
-	if root ==nil {
+func printGivenLev(root *TreeNode, level int, ftr bool) {
+	if root == nil {
 		return
 	}
 	if level == 1 {
-		fmt.Printf("%d  ",root.data)
-	}else if level >1 {
+		fmt.Printf("%d  ", root.data)
+	} else if level > 1 {
 		if ftr {
-			printGivenLev(root.left,level-1,ftr)
-			printGivenLev(root.right,level-1,ftr)
+			printGivenLev(root.left, level-1, ftr)
+			printGivenLev(root.right, level-1, ftr)
 
-		}else {
-			printGivenLev(root.right,level-1,ftr)
-			printGivenLev(root.left,level-1,ftr)
+		} else {
+			printGivenLev(root.right, level-1, ftr)
+			printGivenLev(root.left, level-1, ftr)
 		}
 	}
 
@@ -474,26 +438,457 @@ func printGivenLev(root *TreeNode, level int,ftr bool) {
 
 func spiralPrint(root *TreeNode) {
 	hight := height(root)
-	ftr :=true
-	for i:=1;i<=hight;i++ {
-		printGivenLev(root,i,ftr)
+	ftr := true
+	for i := 1; i <= hight; i++ {
+		printGivenLev(root, i, ftr)
 		ftr = !ftr
 	}
+}
+
+// 变种二： Level order traversal with direction change after every two levels
+/*
+Given a binary tree, print the level order traversal in such a way that first two levels are printed from left to right, next two levels are printed from right to left, then next two from left to right and so on. So, the problem is to reverse the direction of level order traversal of binary tree after every two levels.
+
+Examples:
+
+Input:
+            1
+          /   \
+        2       3
+      /  \     /  \
+     4    5    6    7
+    / \  / \  / \  / \
+   8  9 3   1 4  2 7  2
+     /     / \    \
+    16    17  18   19
+Output:
+1
+2 3
+7 6 5 4
+2 7 2 4 1 3 9 8
+16 17 18 19
+In the above example, first two levels
+are printed from left to right, next two
+levels are printed from right to left,
+and then last level is printed from
+left to right.
+ */
+//方法一： 通过递归实现
+func spiralPrint2(root *TreeNode) {
+	hight := height(root)
+	ftr := true
+	cnt := 0
+	for i := 1; i <= hight; i++ {
+		printGivenLev(root, i, ftr)
+		cnt ++
+		if cnt == 2 {
+			ftr = !ftr
+			cnt = 0
+		}
+		fmt.Println()
+	}
+}
+
+//方法二： 通过非递归实现 O(n)的时间复杂度
+/*
+1. 构建一个堆栈和一个queue，
+2. 首先放入root到queue中，然后通过判断queue.len()>0: queu中的数据已经完全取完，即已经遍历完成
+3. 然后从queue中取出数据，如果righttolef=false，则打印，如果true：则放入到stack中。其他的数据则放入queue中。
+4. 然后判断stack的数据是否为空，如果不为空则取出然后打印，一直到stack为空
+5. 每一层级的遍历都ct++ ,如果ct==2，则righttoleft= !righttoleft，然后ct=0
+ */
+func spiralPrint3(root *TreeNode) {
+	q := queue.New()
+	s := stack.New()
+	qsize := 0
+	ct := 0
+	righttoleft := false
+	if root == nil {
+		return
+	}
+	if root.left == nil && root.right == nil {
+		fmt.Printf("%d  ", root.data)
+	}
+	q.Enqueue(root)
+
+	//首先放入root到queue中，然后通过判断queue.len()>0: queu中的数据已经完全取完，即已经遍历完成
+	for q.Len() > 0 {
+		ct ++
+		//取出q的len，然后遍历queue，qsize代表这一层级的元素个数
+		qsize = q.Len()
+		//取出这一层级的元素
+		for i := 0; i < qsize; i++ {
+			qnode := q.Dequeue().(*TreeNode)
+			//如果righttoleft为false，则打印，否则push到stack中
+			if righttoleft {
+				s.Push(qnode)
+			} else {
+				fmt.Printf("%d  ", qnode.data)
+			}
+			//然后放入下一级的数据到queue中。
+			if qnode.left != nil {
+				q.Enqueue(qnode.left)
+			}
+			if qnode.right != nil {
+				q.Enqueue(qnode.right)
+			}
+		}
+
+		//打印stack中的数据，达到反向打印的目的
+		if righttoleft {
+			for s.Len() > 0 {
+				qnode := s.Pop().(*TreeNode)
+				fmt.Printf("%d  ", qnode.data)
+			}
+		}
+		if ct == 2 {
+			righttoleft = !righttoleft
+			ct = 0
+		}
+		fmt.Println()
+	}
+
 }
 
 /*
 func main() {
 	root := InitTreeNode()
-	spiralPrint(&root)
+	spiralPrint2(&root)
+	fmt.Println("使用queue和stack打印: \n\n")
+	spiralPrint3(&root)
 
 }
 */
 
 //-------------------------------------------------------------------------------------------------------
 
+/*
+广度优先算法
+ */
+//----------------------------------------------------------------------------------------------
+//计算tree的hight，有俩种方法：1，使用递归，2，不适用递归
 /**
-插入一个节点，在为NULL的时候
-*/
-func insert(node *TreeNode, key int) {
 
+方法一： 使用递归计算tree的最大height：沿着根节点遍历到最长的叶子节点的长度
+ */
+func height(root *TreeNode) int {
+	if root == nil {
+		return 0;
+	} else {
+		llen := height(root.left)
+		rlen := height(root.right)
+		if llen < rlen {
+			return rlen + 1
+		} else {
+			return llen + 1
+		}
+	}
+}
+
+/**
+不适用递归实现，借助queue来实现,
+1. 放入root节点，nodecount ++
+2. 循环条件为： nodecount >0 ;取出节点，如果存在left节点，入队，如果存在right节点，入队，然后nodecount--；
+3. 外层循环为nodecount:=queue.Len()
+4. 然后nodecount++
+5. 然后继续取出，知道nodecount ==0时
+ */
+var levelCount int
+
+func heightByQueue(root *TreeNode) int {
+	levelCount = 0
+	q := queue.New()
+	//放入root
+	q.Enqueue(root)
+	for true {
+		//nodecount 当前层的node数量
+		nodecount := q.Len()
+		if nodecount == 0 {
+			return levelCount
+		}
+		//外层循环控制有多少层。当queue中没有节点时，代表所有节点都已经遍历了一遍。
+		levelCount++
+		//从queue中取出节点，当nodecount=0时，代表已经取出该层的节点了，
+		for nodecount > 0 {
+			//node := q.Peek()
+			node := q.Dequeue().(*TreeNode)
+			//如果node存在left节点，则入队，如果存在right节点则入队。
+			if node.left != nil {
+				q.Enqueue(node.left)
+			}
+			if node.right != nil {
+				q.Enqueue(node.right)
+			}
+			nodecount --
+		}
+	}
+	return levelCount
+}
+
+//----------------------------------------------------------------------------------------------
+
+/*
+按照层级打印： breadth first traversal： 即广度优先算法
+1. 先找出tree的深度
+2. 然后根据层级来进行打印
+3. 层级打印： printGivenLevel ： 递归基：level=1,然后打印root.data
+ */
+func printLevel(root *TreeNode) {
+	if root == nil {
+		return
+	}
+	len := height(root)
+
+	for i := 1; i <= len; i++ {
+		printGivenLevel(root, i)
+	}
+}
+
+func printGivenLevel(root *TreeNode, level int) {
+	if root == nil {
+		return
+	}
+	if level == 1 {
+		fmt.Printf("%d ", root.data)
+	} else {
+		printGivenLevel(root.left, level-1)
+		printGivenLevel(root.right, level-1)
+	}
+}
+
+//方法二非递归实现层级打印，借助queue来实现
+func printLevelByQueue(root *TreeNode) {
+	q := queue.New()
+	q.Enqueue(root)
+	for q.Len() > 0 {
+		qnode := q.Dequeue().(*TreeNode)
+		fmt.Printf("%d  ", qnode.data)
+		if qnode.left !=nil {
+			q.Enqueue(qnode.left)
+		}
+		if qnode.right !=nil {
+			q.Enqueue(qnode.right)
+		}
+	}
+}
+
+/*
+func main() {
+	root := InitTreeNode()
+	printLevelByQueue(&root)
+	fmt.Println()
+	printLevelWithoutResc(&root)
+}
+*/
+
+//----------------------------------------------------------------------------------------------
+
+/*
+Print level order traversal line by line | Set 1
+
+Example 2:
+		 1
+	  /     \
+	 2       3
+   /   \       \
+  4     5       6
+	   /  \     /
+	  7    8   9
+Output for above tree should be
+1
+2 3
+4 5 6
+7 8 9<
+
+*/
+// 方法一： 使用递归打印树，时间复杂度： O(n2)
+func printLevelWithResc(root *TreeNode) {
+	if root == nil {
+		return
+	}
+	h := height(root)
+
+	for i := 1; i <= h; i++ {
+		printGivenLevel(root, i)
+		fmt.Println()
+	}
+}
+
+//方法二： 借助queue来实现: O(n)
+func printLevelWithoutResc(root *TreeNode) {
+	q := queue.New()
+	q.Enqueue(root)
+	level := 0
+	for true {
+		nodecount := q.Len()
+		if nodecount == 0 {
+			break
+		}
+		level ++
+
+		for nodecount > 0 {
+			node := q.Dequeue().(*TreeNode)
+			if node.left != nil {
+				q.Enqueue(node.left)
+			}
+			if node.right != nil {
+				q.Enqueue(node.right)
+			}
+			fmt.Printf("%d   ", node.data)
+			nodecount --
+		}
+		fmt.Println()
+	}
+}
+/*
+func main() {
+	root := InitTreeNode()
+	fmt.Printf("递归height：   %d\n",height(&root))
+	fmt.Printf("非递归height：   %d\n",heightByQueue(&root))
+
+	root = TreeNode{}
+	//-----------层级打印--------------------
+	fmt.Println("非递归层级打印：   ")
+	printLevelWithoutResc(&root)
+	fmt.Println("\n\n")
+	fmt.Println("非递归层级打印：   ")
+	printLevelWithResc(&root)
+}
+*/
+
+//---------------------------------------------------------------------------------------------
+/*
+反向打印level order travelal的数据
+   					1
+		2						3
+	4			5				6		7
+8       9   10     11       12     13
+
+打印的结果：
+8 9 10 11 12 13
+4  5 6 7
+2  3
+1
+*/
+//使用传统方式打印
+func reverseLevelOrder(root *TreeNode) {
+	hgt := height(root)
+	for i:=hgt ;i>0 ;i-- {
+		printGivenLev(root,i,true)
+		fmt.Println()
+	}
+}
+
+
+//采用stack方式
+func reverseLevelOrderByStack(root *TreeNode) {
+	q :=queue.New()
+	s :=stack.New()
+	if root == nil {
+		return
+	}
+	if root.right ==nil && root.left ==nil {
+		fmt.Printf("%d  ",root.data)
+	}
+
+	q.Enqueue(root)
+	//s.Push(root)
+	//for q.Len() >0 {
+	//	qsize := q.Len()
+	//	for i :=0;i<qsize;i++ {
+	//		qnode := q.Dequeue().(*TreeNode)
+	//
+	//		if qnode.right !=nil {
+	//			s.Push(qnode.right)
+	//			q.Enqueue(qnode.right)
+	//		}
+	//		if qnode.left !=nil {
+	//			s.Push(qnode.left)
+	//			q.Enqueue(qnode.left)
+	//		}
+	//	}
+	//	s.Push(nil)
+	//}
+
+	//通过queue，将所有节点反向压入到stack中，然后取出相关数据即可。
+	for q.Len() >0 {
+		qnode := q.Dequeue().(*TreeNode)
+		s.Push(qnode)
+		if qnode.right !=nil{
+			q.Enqueue(qnode.right)
+		}
+		if qnode.left !=nil {
+			q.Enqueue(qnode.left)
+		}
+	}
+
+	for s.Len()>0 {
+		tmp := s.Pop()
+		if tmp !=nil {
+			qnode:=tmp.(*TreeNode)
+			fmt.Printf("%d   ",qnode.data)
+		}else {
+			fmt.Println()
+		}
+	}
+}
+
+
+//Perfect Binary Tree Specific Level Order Traversal
+/*
+https://www.geeksforgeeks.org/perfect-binary-tree-specific-level-order-traversal/
+															1
+								2															3
+					4	    			 	5									6						7
+			8				9		  10 			11					12				13		14				15
+		16		17		18	   19  20 	  21	22		23			24		25		26		27 28	29		30		31
+
+
+Print the level order of nodes in following specific manner:
+
+  1 2 3 4 7 5 6 8 15 9 14 10 13 11 12 16 31 17 30 18 29 19 28 20 27 21 26  22 25 23 24
+i.e. print nodes in level order but nodes should be from left and right side alternatively. Here 1st and 2nd levels are trivial.
+While 3rd level: 4(left), 7(right), 5(left), 6(right) are printed.
+While 4th level: 8(left), 15(right), 9(left), 14(right), .. are printed.
+While 5th level: 16(left), 31(right), 17(left), 30(right), .. are printed.
+
+一次处理俩个节点，都进行入队。
+1. 入队： 第一个节点的左节点，第二个节点的右节点
+2. 入队： 第二个节点的左节点，第一个节点的右节点
+
+ */
+
+func printSpecificLevelOrder(root *TreeNode) {
+	q := queue.New()
+	if root ==nil {
+		return
+	}
+	fmt.Printf("%d  ",root.data)
+	if root.left !=nil  {
+		fmt.Printf("%d   %d   ",root.left.data,root.right.data)
+	}
+
+	q.Enqueue(root.left)
+	q.Enqueue(root.right)
+	left :=true
+	for q.Len() >0 {
+		qnode1 := q.Dequeue().(*TreeNode)
+		qnode2 := q.Dequeue().(*TreeNode)
+
+		fmt.Printf("%d	%d	%d	%d	",qnode1.left.data,qnode2.right.data,qnode1.right.data,qnode2.left.data)
+		if qnode1.left.left!=nil {
+			q.Enqueue(qnode1.left)
+			q.Enqueue(qnode2.right)
+			q.Enqueue(qnode1.right)
+			q.Enqueue(qnode2.left)
+			left=!left
+		}
+	}
+}
+
+func main() {
+	root := InitTreeNode()
+	//reverseLevelOrder(&root)
+	//reverseLevelOrderByStack(&root)
+	printSpecificLevelOrder(&root)
 }
